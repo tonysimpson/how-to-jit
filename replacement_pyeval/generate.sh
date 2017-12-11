ca131986b539a6fd58c15610a60e1acbf75a834a
Tony Simpson <anthony.simpson@sky.uk>
2017-12-11 18:55:43 +0000

    Fixed generate.sh so it will correctly create a file I can build with gcc

--- replacement_pyeval/generate.sh
+++ replacement_pyeval/generate.sh
@@ -1,1 +1,2 @@
-cpp -DCONFIG_64 -D_POSIX_THREADS -I $CPYTHON_SRC/Modules/_decimal/libmpdec/  -I /home/guy/work/cpython/Python/ -I /home/guy/work/cpython -I /home/guy/work/cpython/Include /home/guy/work/cpython/Python/ceval.c | grep -v "^#" | sed s/PyEval_/MyEval_/g > src/myeval.c
+mkdir -p src
+cpp -DPy_BUILD_CORE -I /home/guy/work/cpython/Python/ -I /home/guy/work/cpython/Include -I /home/guy/work/cpython /home/guy/work/cpython/Python/ceval.c | grep -v "^#" | sed s/PyEval_/MyEval_/g > src/myeval.c
