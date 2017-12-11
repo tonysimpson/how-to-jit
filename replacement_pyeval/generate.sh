e9417fecbc5f503800a740eec11e0e9323a11d18
Tony Simpson <anthony.simpson@sky.uk>
2017-12-11 19:09:25 +0000

    Path to cpython source is variable

--- replacement_pyeval/generate.sh
+++ replacement_pyeval/generate.sh
@@ -1,2 +1,2 @@
 mkdir -p src
-cpp -DPy_BUILD_CORE -I /home/guy/work/cpython/Python/ -I /home/guy/work/cpython/Include -I /home/guy/work/cpython /home/guy/work/cpython/Python/ceval.c | grep -v "^#" | sed s/PyEval_/MyEval_/g > src/myeval.c
+cpp -DPy_BUILD_CORE -I $CPYTHON_SRC/Python -I $CPYTHON_SRC/Include -I $CPYTHON_SRC $CPYTHON_SRC/Python/ceval.c | grep -v "^#" | sed s/PyEval_/MyEval_/g > src/myeval.c
